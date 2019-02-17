4155c302e32aabbc0c07ed5fb172a55194187abe
Vaidas <vaidomotiejuno@gmail.com>
2019-02-17 18:34:34 +0200

    first commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Interactive CV</title>
+
+    </head>
+    <body>
+
+    <example-component></example-component>
+
+    <script src="app.js">
+    </script>
+    </body>
+</html>
