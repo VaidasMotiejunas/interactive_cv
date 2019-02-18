c03bf8bd0f4527482f3c07d85096ee37bb2e85fd
Vaidas <vaidomotiejuno@gmail.com>
2019-02-18 18:46:31 +0200

    aparently config is done

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,17 +1,21 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <link rel="stylesheet" type="text/css" href="css/app.css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Interactive CV</title>
 
     </head>
     <body>
 
-    <example-component></example-component>
+    <div id="app">
+        <example-component></example-component>
+    </div>
 
-    <script src="app.js">
+    <script src="js/app.js">
     </script>
     </body>
 </html>
